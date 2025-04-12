8d049fd347f1e23f5d2b4ab399371e8d6f01e8bf
James Connolly <me@j-conn.com>
2025-04-12 18:53:16 -0400

    remove modules from public API and release version 0.2.0

--- src/bin/ssec.rs
+++ src/bin/ssec.rs
@@ -1,23 +1,8 @@
 use clap::Parser;
-use std::process::ExitCode;
-use ssec_cli::cli::{Cli, Command};
-use ssec_cli::{enc, dec};
-
-#[inline]
-fn handle_err(result: Result<(), ()>) -> ExitCode {
-	match result {
-		Ok(()) => ExitCode::SUCCESS,
-		Err(()) => ExitCode::FAILURE
-	}
-}
+use ssec_cli::{cli::Cli, run};
 
 #[tokio::main]
-async fn main() -> ExitCode {
+async fn main() -> std::process::ExitCode {
 	let cli = Cli::parse();
-
-	match cli.command {
-		Command::Enc(args) => handle_err(enc::enc(args).await),
-		Command::Dec(args) => handle_err(dec::dec_file(args).await),
-		Command::Fetch(args) => handle_err(dec::dec_fetch(args).await)
-	}
+	run(cli).await
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-mod file;
-
 pub mod cli;
-pub mod enc;
-pub mod dec;
+
+mod file;
+mod enc;
+mod dec;
 
 const BAR_STYLE: &str = "[{elapsed_precise}] {binary_bytes_per_sec} {bar} {binary_bytes}/{binary_total_bytes} ({eta})";
+
+#[inline]
+fn handle_err(result: Result<(), ()>) -> std::process::ExitCode {
+	match result {
+		Ok(()) => std::process::ExitCode::SUCCESS,
+		Err(()) => std::process::ExitCode::FAILURE
+	}
+}
+
+pub async fn run(cli: cli::Cli) -> std::process::ExitCode {
+	match cli.command {
+		cli::Command::Enc(args) => handle_err(enc::enc(args).await),
+		cli::Command::Dec(args) => handle_err(dec::dec_file(args).await),
+		cli::Command::Fetch(args) => handle_err(dec::dec_fetch(args).await)
+	}
+}

ece2043a75a1c492286b722d5afb5d310d0ddf20
James Connolly <me@j-conn.com>
2025-05-13 13:54:57 -0400

    end to end test of fetch subcommand

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,6 @@
 use rand::{SeedableRng, TryRngCore};
-use crate::cli::{Cli, Command, EncArgs, DecArgs};
+use wiremock::{MockServer, Mock, ResponseTemplate, matchers::method};
+use crate::cli::{Cli, Command, EncArgs, DecArgs, FetchArgs};
 use crate::io::IoBundle;
 use crate::run_with_io;
 
@@ -21,11 +22,11 @@ impl IoBundle for MockStdin {
 }
 
 #[tokio::test]
-async fn end_to_end() {
+async fn end_to_end_file() {
 	let mut rng = rand::rngs::SmallRng::seed_from_u64(RNG_SEED);
 	let tmp = tempfile::tempdir().unwrap();
 	let in_path = tmp.as_ref().join("file1");
-	let out_path = tmp.as_ref().join("file.ssec");
+	let enc_path = tmp.as_ref().join("file.ssec");
 	let dec_path = tmp.as_ref().join("file2");
 
 	let mut contents = vec![0; 100000];
@@ -36,7 +37,7 @@ async fn end_to_end() {
 		Cli {
 			command: Command::Enc(EncArgs {
 				in_file: in_path,
-				out_file: Some(out_path.clone())
+				out_file: Some(enc_path.clone())
 			})
 		},
 		MockStdin("hunter2\n")
@@ -47,7 +48,7 @@ async fn end_to_end() {
 	let result = run_with_io(
 		Cli {
 			command: Command::Dec(DecArgs {
-				in_file: out_path.clone(),
+				in_file: enc_path.clone(),
 				out_file: dec_path.clone()
 			})
 		},
@@ -63,12 +64,82 @@ async fn end_to_end() {
 	let result = run_with_io(
 		Cli {
 			command: Command::Dec(DecArgs {
-				in_file: out_path,
-				out_file: dec_path
+				in_file: enc_path,
+				out_file: dec_path.clone()
 			})
 		},
 		MockStdin("not_hunter2\n")
 	).await;
 
 	assert_eq!(result, std::process::ExitCode::FAILURE);
+
+	let dec_contents_wrong = tokio::fs::read(&dec_path).await.unwrap();
+	assert_eq!(dec_contents, dec_contents_wrong);
+}
+
+#[tokio::test]
+async fn end_to_end_fetch() {
+	let mut rng = rand::rngs::SmallRng::seed_from_u64(RNG_SEED);
+	let tmp = tempfile::tempdir().unwrap();
+	let in_path = tmp.as_ref().join("in_file");
+	let enc_path = tmp.as_ref().join("file.ssec");
+	let dec_path = tmp.as_ref().join("out_file");
+
+	let mut contents = vec![0; 20000];
+	rng.try_fill_bytes(&mut contents).unwrap();
+	tokio::fs::write(&in_path, &contents).await.unwrap();
+
+	let result = run_with_io(
+		Cli {
+			command: Command::Enc(EncArgs {
+				in_file: in_path,
+				out_file: Some(enc_path.clone())
+			})
+		},
+		MockStdin("hunter2\n")
+	).await;
+
+	assert_eq!(result, std::process::ExitCode::SUCCESS);
+
+	let mock_server = MockServer::start().await;
+	let enc_contents = tokio::fs::read(&enc_path).await.unwrap();
+
+	Mock::given(method("GET"))
+		.respond_with(
+			ResponseTemplate::new(200)
+				.set_body_bytes(enc_contents)
+		).mount(&mock_server).await;
+
+	let enc_url = url::Url::parse(&mock_server.uri()).unwrap();
+
+	let result = run_with_io(
+		Cli {
+			command: Command::Fetch(FetchArgs {
+				url: enc_url.clone(),
+				out_file: dec_path.clone()
+			})
+		},
+		MockStdin("hunter2\n")
+	).await;
+
+	assert_eq!(result, std::process::ExitCode::SUCCESS);
+
+	let dec_contents = tokio::fs::read(&dec_path).await.unwrap();
+
+	assert_eq!(contents, dec_contents);
+
+	let result = run_with_io(
+		Cli {
+			command: Command::Fetch(FetchArgs {
+				url: enc_url,
+				out_file: dec_path.clone()
+			})
+		},
+		MockStdin("not_hunter2\n")
+	).await;
+
+	assert_eq!(result, std::process::ExitCode::FAILURE);
+
+	let dec_contents_wrong = tokio::fs::read(&dec_path).await.unwrap();
+	assert_eq!(dec_contents, dec_contents_wrong);
 }
